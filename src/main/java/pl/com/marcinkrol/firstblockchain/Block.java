9646e2bbb4c7184b35480cdde85ffa5ebda473a3
Marcin Krol <krolmarin@gmail.com>
2019-05-12 18:21:05 +0200

    mine block impl add

--- src/main/java/pl/com/marcinkrol/firstblockchain/Block.java
+++ src/main/java/pl/com/marcinkrol/firstblockchain/Block.java
@@ -8,8 +8,9 @@ public class Block {
     private Long timeStamp;
     private String data;
     private String prevHash;
+    private int nonce;
 
-    public Block(String data, String prevHash){
+    public Block(String data, String prevHash) {
         this.data = data;
         this.prevHash = prevHash;
         this.timeStamp = new Date().getTime();
@@ -19,8 +20,19 @@ public class Block {
     private String calculateHash() {
         String calculatedHash = StringUtil.applySha256(
                 prevHash +
-                Long.toString(timeStamp) +
-                data);
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        data);
         return calculatedHash;
     }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined! : " + hash);
+    }
+
 }
--- src/main/java/pl/com/marcinkrol/firstblockchain/FirstBlockChain.java
+++ src/main/java/pl/com/marcinkrol/firstblockchain/FirstBlockChain.java
@@ -12,8 +12,16 @@ public class FirstBlockChain {
     public static void main(String[] args) {
 
         blockChain.add(new Block("First block", "0"));
+        System.out.println("Mining Block 1...");
+        blockChain.get(0).mineBlock(difficuty);
+
         blockChain.add(new Block("Second block", blockChain.get(blockChain.size() - 1).hash));
+        System.out.println("Mining Block 2...");
+        blockChain.get(1).mineBlock(difficuty);
+
         blockChain.add(new Block("Third block", blockChain.get(blockChain.size() - 1).hash));
+        System.out.println("Mining Block 3...");
+        blockChain.get(2).mineBlock(difficuty);
 
         String blockChainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockChain);
         System.out.println(blockChainJson);
